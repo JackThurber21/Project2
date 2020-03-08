fc9b32c0c21a4da73bebcf604a541fd2a3f1fed0
JackThurber21 <45569106+JackThurber21@users.noreply.github.com>
2020-03-08 16:33:03 -0500

    Add files via upload

---
+++ Project2/project2/MainFrame.java
@@ -0,0 +1,25 @@
+package project2;
+
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+
+import javax.swing.JFrame;
+
+
+public class MainFrame {
+	public static void main(String[] args) {
+		JFrame frame = new JFrame("Project 2 GUI");
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		mainPanel panel = new mainPanel();
+		frame.getContentPane().add(panel);
+		
+		frame.pack();
+		frame.setVisible(true);
+		frame.addWindowListener(new WindowAdapter() {
+			public void windowClosing(WindowEvent e) {
+			//	panel.doClose();
+			}
+		});
+	}
+}
